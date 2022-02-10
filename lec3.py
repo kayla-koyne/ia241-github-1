76e0af200092422442d5799c6de8433ca5d6cc34
EC2 Default User <ec2-user@ip-172-31-94-42.ec2.internal>
2022-02-10 03:04:44 +0000

    added my lab3.py file.

---
+++ lec3.py
@@ -0,0 +1,15 @@
+'''
+lec 3
+'''
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list)
+
+my_nested_list = [1, 2, 3, [1, 2, 3, 4, 5 ]]
+
+print(my_nested_list)
+
+my_list[0] = 6
+print(my_list)
+print("hello/tworld")
